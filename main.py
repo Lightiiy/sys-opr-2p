e8c5c7ef2d94a5626f4aa7283fc93065eb4be9b5
Gabriel Bekier <gabrielbekier@gmail.com>
2023-04-15 21:49:08 +0200

    pygame - empty game window

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ import time
 from os import listdir
 from os.path import isfile, join 
 
+BG_COLOR = (255,255,255)
+WIDTH, HEIGHT = 640, 640
+FPS = 60
+P_VELOCITY = 5
+
 # DEFINE functions used for threading
+py.init()    
+py.display.set_caption("Scalar")
+window = py.display.set_mode((WIDTH,HEIGHT))
+
 def DUPA_function():
     print("DUPA_1")
     time.sleep(3)
@@ -18,22 +27,39 @@ def TEST_function():
     time.sleep(6)
     print("TESTING 2")
 
+def main(window):
+    clock =  py.time.Clock()
+
+    run = True
+    while run:
+      clock.tick(FPS)
+
+      for event in py.event.get():
+          if event.type == py.QUIT:
+              run = False
+              break
+          
+    py.quit()
+    quit()
+
 
 if __name__ == "__main__":
-# DEFINE a list of threads to join
-    all_threads = list()
-    for x in range(5):
-      print(x)
-# CREATE a thread and append it to a variable
-      dupa = threading.Thread(target=DUPA_function)
-      test = threading.Thread(target=TEST_function)
-# APEND variable to the list of threads
-      all_threads.append(dupa)
-      all_threads.append(test)
-# STARTS the threads 
-      dupa.start()
-      test.start()
-# END the threads at the end of MAIN thread (program's thread)
-    for index, thread in enumerate(all_threads):
-        thread.join()
+    main(window)
 
+
+# # DEFINE a list of threads to join
+#     all_threads = list()
+#     for x in range(5):
+#       print(x)
+# # CREATE a thread and append it to a variable
+#       dupa = threading.Thread(target=DUPA_function)
+#       test = threading.Thread(target=TEST_function)
+# # APEND variable to the list of threads
+#       all_threads.append(dupa)
+#       all_threads.append(test)
+# # STARTS the threads 
+#       dupa.start()
+#       test.start()
+# # END the threads at the end of MAIN thread (program's thread)
+#     for index, thread in enumerate(all_threads):
+#         thread.join()
